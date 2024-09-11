082a8f2b98103af6718ebe5fc9ec8894b12cc2a7
TINHOLINDO <134516651+TINHOLINDO@users.noreply.github.com>
2024-09-11 11:13:54 -0300

    Add files via upload

--- reconhecimentoVoz.js
+++ reconhecimentoVoz.js
@@ -8,6 +8,7 @@ recognition.lang = 'pt-br'
 recognition.start()
 
 recognition.addEventListener('result', onSpeak)
+recognition.addEventListener('end', () => recognition.start())
 
 function onSpeak(e){
     chute = e.results[0][0].transcript
